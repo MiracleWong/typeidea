1c387067325886cc801cfcd7a63a7662c55238da
MiracleWong <cfwr1991@126.com>
2020-03-30 20:00:47 +0800

    6.2.1 自定义list页面

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -30,18 +30,36 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    '''
+    自定义过滤器，只展示当前用户的分类
+    '''
+
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     # list_display 配置列表页面展示哪些字段
     list_display = [
         'title', 'category', 'status',
-        'created_time', 'operator'
+        'created_time', 'operator',
     ]
 
     # list_display 哪些字段，可以作为链接，点击进入编辑界面
     list_display_links = []
 
-    list_filter = ['category', ]
+    list_filter = [CategoryOwnerFilter, ]
     search_fields = ['title', 'category__name']
 
     actions_on_top = True
@@ -69,6 +87,10 @@ class PostAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner= request.user)
 
     def post_count(self, obj):
         return obj.post_set.count()
