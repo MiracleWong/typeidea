6bb602b15567756973f9f9271556255263385c84
MiracleWong <cfwr1991@126.com>
2020-04-02 09:03:31 +0800

    7.1.6-7 编写View代码，配置模板数据

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,13 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post, Tag, Category
 
 
 # Create your views here.
 
 
+# def post_list(request, category_id=None, tag_id=None):
+#     if tag_id:
+#         print("tag_id:"+tag_id)
+#         try:
+#             tag = Tag.objects.get(id=tag_id)
+#             # print("tag:"+tag)
+#         except tag.DoesNotExist:
+#             post_list = []
+#         else:
+#             post_list = tag.post_set.filter(status=Post.STATUS_NORMAL)
+#     else:
+#         post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
+#         if category_id:
+#             post_list = post_list.filter(category_id=category_id)
+#
+#     return render(request, 'blog/list.html', context={'post_list': 'post_list'})
+#
+#
+# def post_detail(request, post_id=None):
+#     try:
+#         post = Post.objects.get(id=post_id)
+#     except Post.DoesNotExist:
+#         post = None
+#     return render(request, 'blog/detail.html', context={'post': 'post'})
 def post_list(request, category_id=None, tag_id=None):
-    return render(request, 'blog/list.html', context={'name': 'post_list'})
+    queryset = Post.objects.all()
+    if category_id:
+        # 分类页面
+        queryset = queryset.filter(category_id=category_id)
+    elif tag_id:
+        # 标签页面
+        try:
+            tag = Tag.objects.get(id=tag_id)
+        except Tag.DoesNotExist:
+            queryset = []
+        else:
+            queryset = tag.posts.all()
+
+    context = {
+        'posts': queryset,
+    }
+    return render(request, 'blog/list.html', context=context)
 
 
-def post_detail(request, post_id=None):
-    return render(request, 'blog/detail.html', context={'name': 'post_detail'})
+def post_detail(request, pk=None):
+    try:
+        post = Post.objects.get(pk=pk)
+    except Post.DoesNotExist:
+        post = None
+
+    context = {
+        'post': post,
+    }
+    return render(request, 'blog/detail.html', context=context)
