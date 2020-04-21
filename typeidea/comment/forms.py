ad723beda98d97ec91c3d81fad262fd9f47a68df
MiracleWong <cfwr1991@126.com>
2020-04-21 16:43:03 +0800

    9.4.3.1 修改无法添加评论的Bug，修改后台无法点击comment评论显示的Bug

--- typeidea/comment/forms.py
+++ typeidea/comment/forms.py
@@ -40,4 +40,4 @@ class CommentForm(forms.ModelForm):
 
     class Meta:
         model = Comment
-        fields= ['nickname', 'email', 'website', 'content']
+        fields = ['nickname', 'email', 'website', 'content']
